bfe9500cf8fa039fc19a7302ab31a9088b83ea42
liton61 <litonofficial61@gmail.com>
2024-04-16 12:46:36 +0600

    delete method added for cattle

--- index.js
+++ index.js
@@ -267,6 +267,14 @@ async function run() {
             res.send(result);
         })
 
+        // delete method for cattle
+        app.delete('/cattle/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cattleCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
